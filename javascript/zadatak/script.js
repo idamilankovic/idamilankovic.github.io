9c55ea9670ed5cc300fffe0d48a4c5a12a128f73
idamilankovic <ida@itgrow.hr>
2023-01-26 14:47:58 +0100

    final3
    Signed-off-by: idamilankovic <ida@itgrow.hr>

--- javascript/zadatak/script.js
+++ javascript/zadatak/script.js
@@ -1,13 +1,11 @@
 var apiKey = "c3679095f19f4a9dadfa02f59f24f360";
-startDate = new Date();
-endDate = new Date();
-endDate.setDate(endDate.getDate() -1);
-startDate.setDate(startDate.getDate()-7);
 
-function weather () {
-    var city = cityValue ();
+
+function getDropdownValues () {      
+    /** Funkcija vraća vrijednosti grada i akcije iz padajućih izbornika */
+    var city = cityName ();
     var action = actionName();
-    get(city, startDate, endDate, apiKey, action);
+    setDate(city, apiKey, action);
 }
 
 function actionName() {
@@ -16,54 +14,64 @@ function actionName() {
     return action;
 }
 
-function cityValue(){
+function cityName(){
     var selectCity = document.querySelector("#cityName");
-    var cityValue = selectCity.value;
-    return cityValue;
+    var city = selectCity.value;
+    return city;
 };
 
-function get (city, startDate, endDate, apiKey, action) {
+function setDate (city, apiKey, action) {
+    /** Funkcija postavlja početni i konačni datum za poziv */
     if (action == "current") {
+        /** Ako je odabrana akcija "current" onda je početni datum današnji, a konačni datum uvećan za 1. Na taj način se dobije poziv za današnji datum. */
         var startDate = new Date().toJSON().slice(0,10);
         var endDate = new Date ();
         endDate.setDate(endDate.getDate() + 1);
-        endDate = endDate.toJSON().slice(0,10);       
-        getJson(city, startDate, endDate, apiKey, action);       
-    }
-    else if (action == "weekly_min" || action == "weekly_max" || action == "weekly_avg") {
-        getJson(city, startDate.toJSON().slice(0,10), endDate.toJSON().slice(0,10), apiKey, action);  
+        endDate = endDate.toJSON().slice(0,10);   
+        getWeatherData(city, startDate, endDate, apiKey, action);       
     }
     else {
-        return alert("Odaberi grad i prikaz temperature")
+        /** Ako je odabrana bilo koja akcija osim "current", onda je današnji datum umanjen za 7 dana kako bi se dobio početni datum i današnji datum je umanjen za jedan dan kako bi se dobio konačni datum. */
+        startDate = new Date();
+        endDate = new Date();
+        startDate.setDate(startDate.getDate()-7);
+        startDate = startDate.toJSON().slice(0,10);
+        endDate.setDate(endDate.getDate() -1);
+        endDate = endDate.toJSON().slice(0,10);   
+        getWeatherData(city, startDate, endDate, apiKey, action);  
     }
 }
 
-function getJson (city, startDate, endDate, apiKey, action) {
+function getWeatherData (city, startDate, endDate, apiKey, action) {
+    /** Poziv servisa */
     const request = new XMLHttpRequest();
     url = "https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&city=" + city +"&start_date=" + startDate + "&end_date="+ endDate + "&key=" + apiKey;
     request.open("GET", url);
     request.send();
     request.onload = () => {
-    if (request.status ===200) {
+        /**Pristup dobivenim podacima nakon poziva */
+    if (request.status === 200) {
+        /** Provjera statusa requesta*/
         if (action == "current") {
+            /** Iz JSON-a dobivamo trenutnu temperatuu i njenu vrijednost dodajemo elementu s id=content*/
             var response = JSON.parse(request.response);
             var current = response.data[0].temp;
             document.querySelector("#content").value = current;
         }
         else if (action == "weekly_min") {
+            /** Iz JSON-a dobivamo minimalnu tjednu temperaturu i njenu vrijednost dodajemo elementu s id=content */
             var response = JSON.parse(request.response);
             var min_num = 500;
             var res = response.data;
             for (var k in res) {
-                for (var i in res[k]) {
-                    if (res[k].min_temp < min_num) {
-                        min_num = res[k].min_temp;
-                    }
+                if (res[k].min_temp < min_num) {
+                    min_num = res[k].min_temp;
                 }
             }
             document.querySelector("#content").value = min_num;
         }
         else if (action == "weekly_max") {
+            /** Iz JSON-a dobivamo maksimalnu tjednu temperaturu i njenu vrijednost dodajemo elementu s id=content */
             var response = JSON.parse(request.response);
             var max_num = -273;
             var res = response.data;
@@ -77,6 +85,7 @@ function getJson (city, startDate, endDate, apiKey, action) {
             document.querySelector("#content").value = max_num;
         }
         else if (action = "weekly_avg") {
+            /** Iz JSON-a dobivamo dnevne temperature za zadnjih 7 dana, izračunamo prosjek i njegovu vrijednost dodajemo elementu s id=content */
             var response = JSON.parse(request.response);
             var avg = 0;
             var res = response.data;
@@ -86,10 +95,10 @@ function getJson (city, startDate, endDate, apiKey, action) {
                 var avg_temp = (avg/7).toFixed(2);
                 document.querySelector("#content").value = avg_temp;
         }
-            return response
         }
         else {
-            console.log(`error ${request.status}`)
+            /**Request status nije 200 te vraća allert poruku sa statusom requesta */
+            alert(`Error ${request.status}`)
         }
     };
 };
