9798ffad26bbac664927de2d718357e88523aeb3
idamilankovic <ida@itgrow.hr>
2023-02-16 11:37:34 +0100

    final
    Signed-off-by: idamilankovic <ida@itgrow.hr>

--- react_zad/src/App.js
+++ react_zad/src/App.js
@@ -1,13 +1,10 @@
-import { Box } from '@mui/system';
-import City from './components/city';
-import AppBar from '@mui/material/AppBar'
+import GetCurrentTemp from './components/getCurrentTemperature';
 
 function App() {
-
   return (
     <div className='container'>
       <h1>Odaberi grad za prikaz temperature:</h1>
-      <City />
+      <GetCurrentTemp />
     </div>
   );
 }
